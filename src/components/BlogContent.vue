cf067dcd35a9026a920bebc64201f3fa6da5f46b
wdsuperman <425233548@qq.com>
2018-05-16 11:59:48 +0800

    2

--- src/components/BlogContent.vue
+++ src/components/BlogContent.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
-    <div v-html="htmlPost"></div>
+    <div v-html="htmlPost">
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+import md from '../utils/markdownUtils'
 export default {
   name:'BlogContent',
   data:()=>({
     post:''
   }),
   created(){
-    const id = this.$toute.params.id
+    const id = this.$route.params.id
     const uri =`https://raw.githubusercontent.com/wdsuperman/markdown/master/data/posts/${id}.md`
     axios.get(uri).then(res => {
-      console.log(res.data)
+      // console.log(res.data)
+      this.post = res.data
     })
+  },
+  computed:{
+    htmlPost(){
+      return md.render(this.post)
+    }
   }
 }
 </script>
---
+++ src/utils/markdownUtils.js
@@ -0,0 +1,16 @@
+import MarkdownIt from 'markdown-it'
+import hljs from 'highlight.js'
+ 
+ export default new MarkdownIt({
+   highlight: function(str, lang) {
+     if (lang && hljs.getLanguage(lang)) {
+       try {
+         return hljs.highlight(lang, str).value
+       } catch (__) {
+         return null
+       }
+     }
+ 
+     return ''
+   }
+ })
